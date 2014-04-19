c95c0ec0135ed4465a7a790e3267da6a8fdfe627
Shawn Sparks <shawn.sparks@marshmallowswisdom.com>
2014-04-18 20:59:18 -0500

    added a post example

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ app.use( express.cookieParser() );
 app.use( express.session( { secret: 'whatever' } ) );
 app.use( express.bodyParser() );
 app.use( '/ajax', express.static( __dirname ) );
+app.post( '/demo', function( request, response ) { response.send( 'success' ); } );
 
 var server = app.listen( 8080, function() {
 	console.log( 'Listening on port %d', server.address().port );
